3a37811e46a7609607aac62df145ddf9d3f4712e
RJ Meijer <pibara@gmail.com>
2023-04-20 17:01:21 +0200

    latest try

--- demo2/fluree_parts/enforce_trias_politica/hasNoConflictingPowers.clj
+++ demo2/fluree_parts/enforce_trias_politica/hasNoConflictingPowers.clj
@@ -1,1 +1,52 @@
-(== (userHasRole01 (?o) "judicial") 1)
+(not
+  (or  
+    (and 
+      (== 
+        (userHasRole
+          (get (?o) "_user/username")
+          "executive"
+        )
+        1
+      ) 
+      (== 
+        (userHasRole 
+          (get (?o) "_user/username") 
+          "judicial"
+        )
+        1
+      )
+    )
+    (and 
+      (== 
+        (userHasRole
+          (get (?o) "_user/username")
+          "executive"
+        )
+        1
+      ) 
+      (==
+        (userHasRole
+          (get (?o) "_user/username")
+          "legislator"
+        )
+        1
+      )
+    )
+    (and 
+      (==
+        (userHasRole
+          (get (?o) "_user/username")
+          "judicial"
+        )
+        1
+      ) 
+      (== 
+        (userHasRole
+          (get (?o) "_user/username")
+          "legislator"
+        )
+        1
+      )
+    )
+  )
+)
